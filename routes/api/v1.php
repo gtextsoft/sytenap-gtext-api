6e092bb52e855cf97100e8f3ff2178abaeaade95
Oyewale Abdulganiyu <olawaleabdulganiyujabulani@gmail.com>
2026-01-22 11:52:05 +0100

    protection

--- routes/api/v1.php
+++ routes/api/v1.php
@@ -117,8 +117,11 @@ Route::prefix('v1')->group(function () {
     // Public documents
     Route::get('documents', [DocumentController::class, 'index']);
     //Route::get('documents/{id}/download', [DocumentController::class, 'download']);
-    Route::get('/documents/{document}/download', [DocumentController::class, 'download'])
-        ->name('documents.download');
+   Route::middleware('auth:sanctum')->get('/documents/{document}/download', [DocumentController::class, 'download'])
+    ->name('documents.download');
+
+
+
 
     // -------------------------
     // Admin Routes
