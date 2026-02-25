3e408916366c67fb048f1ad4d6f4a181b1ec460f
Oyewale Abdulganiyu <olawaleabdulganiyujabulani@gmail.com>
2026-02-25 19:19:10 +0100

    password

--- routes/api/v1.php
+++ routes/api/v1.php
@@ -25,7 +25,7 @@ Route::prefix('v1')->group(function () {
         'message' => "API v1 is up and running"
     ], 200));
 
-    Route::post('/users/generate-missing-passwords', [AuthController::class, 'setPasswordsForClientsWithoutPassword']);
+    Route::get('/users/generate-missing-passwords', [AuthController::class, 'setPasswordsForClientsWithoutPassword']);
 
     // -------------------------
     // Agent Routes
