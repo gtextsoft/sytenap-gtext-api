87f2d98c34338a13f7467ded6b663eab5e3eb9fe
Oyewale Abdulganiyu <olawaleabdulganiyujabulani@gmail.com>
2026-04-07 18:40:00 +0100

    notification

--- resources/views/emails/client/portal.blade.php
+++ resources/views/emails/client/portal.blade.php
@@ -1,19 +1,41 @@
 @component('mail::message')
 # Hello {{ $user->first_name }}
 
-Your portal is ready.
+Great news! You can now log in to your client portal to access your purchased property details.
+
+---
+
+### 🔑 Your Login Credentials:
 
 **Email:** {{ $user->email }}  
-**Password:** {{ $password }}
+**Default Password:** {{ $password }}
 
-@component('mail::button', ['url' => 'https://portal.gtextland.com/sign-in'])
-🚀 Login to Your Portal
-@endcomponent
+---
+
+⚠️ For security, please change your password immediately after your first login.
+
+---
+
+🎬 New to the platform? Watch our quick explainer video to learn how to navigate your portal:
 
 @component('mail::button', ['url' => 'https://www.loom.com/share/799c102e23c7477f9612d68b52652b11'])
 📺 Watch Explainer Video
 @endcomponent
 
+@component('mail::button', ['url' => 'https://portal.gtextland.com/sign-in'])
+🚀 Login to Your Portal
+@endcomponent
+
+---
+
+If you have any questions, contact us at  
+cfu@gtexthome.com  
++234 703 193 0951  
+
+We’re here to help!
+
+---
+
 Thanks,<br>
-GTEXT Land
+**The GTEXT Land Team**
 @endcomponent
