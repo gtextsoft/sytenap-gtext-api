c32be256d5dc0a1893edff997706e1871f20ee2a
Oyewale Abdulganiyu <olawaleabdulganiyujabulani@gmail.com>
2026-05-09 02:42:58 +0100

    user role fix

--- database/migrations/2025_01_10_000004_create_user_role_table.php
+++ database/migrations/2025_01_10_000004_create_user_role_table.php
@@ -13,11 +13,21 @@ return new class extends Migration
     {
         Schema::create('user_role', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
-            $table->foreignId('role_id')->constrained('roles')->onDelete('cascade');
+
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('role_id');
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
+
+            $table->foreign('role_id')
+                ->references('id')
+                ->on('roles')
+                ->onDelete('cascade');
+
             $table->timestamps();
-            
-            $table->unique(['user_id', 'role_id']);
         });
     }
 
